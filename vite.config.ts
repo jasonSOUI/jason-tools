bb0d1d443e9dc5c1860066f98ba80ab1ea95047e
黃俊傑 <33945741+jasonSOUI@users.noreply.github.com>
2025-10-28 13:07:30 +0800

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/tools/', 
-  server: {
-    host: '0.0.0.0',           // 如果希望對外也能訪問（選用）
-    port: 5173,                // 根據您的 port
-    allowedHosts: ['www.soui.com.tw'],
-  }
+  plugins: [react()]
 })
