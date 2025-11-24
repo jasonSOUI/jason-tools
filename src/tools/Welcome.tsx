c18b8f337123dca6263adbd97c0028a8c1e43e55
黃俊傑 <33945741+jasonSOUI@users.noreply.github.com>
2025-11-24 10:15:48 +0800

    Update Welcome.tsx

--- src/tools/Welcome.tsx
+++ src/tools/Welcome.tsx
@@ -4,6 +4,7 @@ const Welcome: React.FC = () => {
   return (
     <div>
       <p>請從左側選單選擇一個工具開始使用。</p>
+      <a href="https://github.com/jasonSOUI/LifeDocument/blob/main/%E7%AF%84%E4%BE%8B.md">範例</a>
     </div>
   );
 };
